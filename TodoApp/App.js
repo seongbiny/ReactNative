ddc08f3d5633cefaccaaf2c018f3dd7eac30777b
Yun Seongbin <60650518+seongbiny@users.noreply.github.com>
2022-09-05 22:18:01 +0900

    feat:220905

---
+++ TodoApp/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
+import DateHead from './components/DateHead';
+import Empty from './components/Empty';
+
+function App() {
+  const today = new Date();
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
+        <DateHead date={today} />
+        <Empty />
+        <AddTodo />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
+
+export default App;
---
+++ TodoApp/components/DateHead.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+function DateHead({date}) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  const {top} = useSafeAreaInsets();
+
+  const formatted = `${year}년 ${month}월 ${day}일`;
+  return (
+    <>
+      <View style={[styles.statusBarPlaceholder, {height: top}]} />
+      <View style={styles.block}>
+        <Text style={styles.dateText}>{formatted}</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  statusBarPlaceholder: {
+    backgroundColor: '#26a69a',
+  },
+  block: {
+    padding: 16,
+    backgroundColor: '#26a69a',
+  },
+  dateText: {
+    fontSize: 24,
+    color: 'white',
+  },
+});
+
+export default DateHead;
